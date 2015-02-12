71547b71f3605359a3fc8adc210844681bd4a611
Beppu Kaoru <beppu@f.ait.kyushu-u.ac.jp>
2015-02-13 01:36:55 +0900

    make wikipedia search

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+var module = angular.module("App", ["ionic"]);
+module.controller("mainCtrl", ["$scope", "$http", function($scope, $http) {
+	$scope.nowInput = false;
+	$scope.keyword = "";
+	$scope.startInput = function() {
+		$scope.nowInput = true;
+	};
+	$scope.finishInput = function() {
+		$scope.nowInput = false;
+		var uri = "http://wikipedia.simpleapi.net/api?"
+			+ "keyword=" + encodeURIComponent($scope.keyword)
+			+ "&output=json"
+			+ "&lang=ja"
+			+ "&search=1"
+			+ "&callback=JSON_CALLBACK";
+		$http.jsonp(uri).success(function(data) {
+			console.log(data);
+			$scope.results = data;
+			if(data == null) {
+				$scope.results = [];
+			}
+		});
+	};
+}]);
+
+module.controller("detailCtrl", ["$scope", "$ionicModal", function($scope, $ionicModal) {
+	$ionicModal.fromTemplateUrl("detail.html", {
+		scope: $scope,
+		animation: "slide-in-up"
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	$scope.openModal = function(result) {
+		$scope.selectedResult = result;
+		$scope.bodyArr = $scope.selectedResult.body.split(/<br\s*[\/]?>/gi);
+		$scope.modal.show();
+	};
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+}]);
